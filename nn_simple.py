1f81a5182588dfb659a68fb64d53295e259a9145
yaceben <19843412+yaceben@users.noreply.github.com>
2021-03-24 22:08:36 -0300

    correction de la fonction test

--- nn_simple.py
+++ nn_simple.py
@@ -213,9 +213,9 @@ def test(dataloader, model):
     test_loss, correct = 0, 0
     with torch.no_grad():
         for X, y in dataloader:
-            X, y = X.to(device), y.to(device)
+            #X, y = X.to(device), y.to(device)
             pred = model(X)
-            test_loss += loss_fn(pred, y).item()
+            test_loss += model.loss_fn(pred, y).item()
             correct += (pred.argmax(1) == y).type(torch.float).sum().item()
     test_loss /= size
     correct /= size
